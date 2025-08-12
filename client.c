0f81c8f710f1becd4d902719fea125dbc095d8f4
M10ky <miokyrakotoarivelo@gmail.com>
2025-08-13 00:25:54 +0300

    Mandatory && error bonus

--- client.c
+++ client.c
@@ -6,62 +6,107 @@
 /*   By: miokrako <miokrako@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 21:29:33 by miokrako          #+#    #+#             */
-/*   Updated: 2025/08/07 08:44:18 by miokrako         ###   ########.fr       */
+/*   Updated: 2025/08/08 13:46:20 by miokrako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-volatile	sig_atomic_t ack = 0;
-void	ack_handler(int sig)
+volatile sig_atomic_t ack = 0; // Variable globale de synchronisation
+
+void ack_handler(int sig)
 {
 	(void)sig;
-	ack = 1;
+	ack = 1; // Le serveur a confirmé réception du bit
 }
 
-void	wait_ack()
+void send_char(pid_t pid, char c)
 {
-	while (!ack)
-		pause();
-	ack = 0;
-}
+    int i = 0;
+    while (i < 8)
+    {
+        ack = 0; // Réinitialiser l’ACK avant chaque bit
 
-void	send_character(__pid_t pid, char c)
-{
-	int i;
+        if ((c >> i) & 1)
+            kill(pid, SIGUSR2); // bit = 1
+        else
+            kill(pid, SIGUSR1); // bit = 0
 
-	i = 0;
-	while (i < 8)
-	{
-		if ((c >> i) & 1)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		wait_ack();
-		usleep(50);
-		i++;
-	}
-}
+        int attempts = 0;
+        while (!ack)
+        {
+            // Vérifier si le serveur est toujours vivant
+            if (kill(pid, 0) == -1)
+            {
+                write(2, "\nError: Server is not responding\n", 33);
+                exit(1);
+            }
 
-int	main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		printf("Usage: %s <PID> <message>\n", argv[0]);
-		return 1;
+            // Attente courte avant de réessayer
+            usleep(5000); // 5 ms
+
+            // Timeout après ~1 seconde
+            if (++attempts > 200)
+            {
+                write(2, "\nError: No ACK from server\n", 28);
+                exit(1);
+            }
+        }
+        i++;
     }
-	__pid_t pid = ft_atoi(argv[1]);
-	const char *msg = argv[2];
-	struct sigaction sa;
-	sa.sa_handler = ack_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGUSR1, &sa, NULL);
-	while (*msg)
-	{
-		send_character(pid, *msg);
-		msg++;
-	}
-	send_character(pid, '\0');
-	return 0;
+}
+
+
+int is_numeric(const char *str)
+{
+    while (*str)
+    {
+        if (*str < '0' || *str > '9')
+            return 0;
+        str++;
+    }
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        write(2, "Usage: ./client <PID> <MESSAGE>\n", 33);
+        return 1;
+    }
+
+    if (!is_numeric(argv[1]))
+    {
+        write(2, "Error: PID must be numeric\n", 28);
+        return 1;
+    }
+
+    pid_t pid = atoi(argv[1]);
+    if (pid <= 1 || kill(pid, 0) == -1)
+    {
+        write(2, "Error: Invalid PID\n", 20);
+        return 1;
+    }
+
+    // Associer SIGUSR1 au handler ACK
+    struct sigaction sa;
+    sa.sa_handler = ack_handler;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        return 1;
+    }
+
+    const char *msg = argv[2];
+    while (*msg)
+    {
+        send_char(pid, *msg);
+        msg++;
+    }
+    send_char(pid, '\0');
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -6,73 +6,98 @@
 /*   By: miokrako <miokrako@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 21:29:50 by miokrako          #+#    #+#             */
-/*   Updated: 2025/08/07 08:42:21 by miokrako         ###   ########.fr       */
+/*   Updated: 2025/08/08 13:36:55 by miokrako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_receive g_data = {NULL, 0, 0, 0};
-
-void	append_char(char c)
+typedef struct s_msg
 {
-	size_t	old_len = 0;
-	char	*new_msg;
+	char	*str;
+	int		bit_count;
+	char	c;
+	pid_t	client_pid;
+}	t_msg;
 
-	if (g_data.message)
-		old_len = ft_strlen(g_data.message);
+t_msg g_msg = {NULL, 0, 0, 0};
 
-	new_msg = malloc(sizeof(char) * (old_len + 2)); // +1 pour c, +1 pour \0
-	if (!new_msg)
-		exit(EXIT_FAILURE);
+void reset_message(void)
+{
+	free(g_msg.str);
+	g_msg.str = NULL;
+	g_msg.bit_count = 0;
+	g_msg.c = 0;
+}
 
-	if (g_data.message)
+void append_char(char c)
+{
+	size_t len = g_msg.str ? strlen(g_msg.str) : 0;
+	char *new_buf = malloc(len + 2);
+	if (!new_buf)
 	{
-		strcpy(new_msg, g_data.message);
-		free(g_data.message);
+		write(2, "Allocation error\n", 17);
+		exit(1);
 	}
-	else
-		new_msg[0] = '\0';
-
-	new_msg[old_len] = c;
-	new_msg[old_len + 1] = '\0';
-	g_data.message = new_msg;
+	if (g_msg.str)
+	{
+		memcpy(new_buf, g_msg.str, len);
+		free(g_msg.str);
+	}
+	new_buf[len] = c;
+	new_buf[len + 1] = '\0';
+	g_msg.str = new_buf;
 }
 
 void handler(int sig, siginfo_t *info, void *context)
 {
 	(void)context;
-	g_data.client_pid = info->si_pid;
-	if (sig == SIGUSR2)
-		g_data.current_char |= (1 << g_data.bit_count);
-	g_data.bit_count++;
-	if (g_data.bit_count == 8)
+
+	if (g_msg.client_pid != info->si_pid)
 	{
-		append_char(g_data.current_char);
-		if (g_data.current_char == '\0')
-		{
-			printf("Message reçu : %s\n", g_data.message);
-			free(g_data.message);
-			g_data.message = NULL;
-		}
-		g_data.bit_count = 0;
-		g_data.current_char = 0;
+		g_msg.client_pid = info->si_pid;
+		reset_message();
 	}
-	kill(g_data.client_pid, SIGUSR1);
+
+	if (sig == SIGUSR2) // bit = 1
+		g_msg.c |= (1 << g_msg.bit_count);
+	g_msg.bit_count++;
+
+	if (g_msg.bit_count == 8)
+	{
+		if (g_msg.c == '\0')
+		{
+			printf("Message reçu : %s\n", g_msg.str ? g_msg.str : "");
+			fflush(stdout);
+			reset_message();
+		}
+		else
+		{
+			append_char(g_msg.c);
+		}
+		g_msg.c = 0;
+		g_msg.bit_count = 0;
+	}
+
+	kill(info->si_pid, SIGUSR1);
 }
 
 int main(void)
 {
 	struct sigaction sa;
 
+	printf("Serveur PID : %d\n", getpid());
+
 	sa.sa_sigaction = handler;
-	sa.sa_flags = SA_SIGINFO | SA_RESTART;
+	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-
-	printf("PID du serveur : %d\n", getpid());
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 ||
+		sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		return 1;
+	}
 
 	while (1)
 		pause();
